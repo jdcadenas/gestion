c37674e2210108e2124833f2e36b41e180ef50b7
José Cadenas <jdcadenas@gmail.com>
2024-04-25 22:39:31 -0400

    Add MoodleServiceProvider to bootstrap providers
    This commit adds the class "App\Providers\MoodleServiceProvider" to the array of providers in the "bootstrap/providers.php" file.

---
+++ app/Providers/MoodleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Routing\Route;
+use Illuminate\Support\ServiceProvider;
+
+class MoodleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(MoodleServiceInterface::class, MoodleService::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Route::post('/moodle/login', function (Request $request) {
+            $moodleService = app(MoodleServiceInterface::class);
+            $response = $moodleService->loginTeacher($request->username, $request->password);
+            return response()->json($response);
+        });
+    }
+}
